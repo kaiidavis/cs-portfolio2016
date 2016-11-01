c22371ad1a5ff28d00c376cd2bc19f948271f9b0
schoolworkshasta <kdavis.sh@mysummitps.org>
2016-11-01 22:30:33 +0000

    update

--- calc.js
+++ calc.js
@@ -22,28 +22,80 @@ var buttonminus = document.getElementById("buttonminus");
 var buttonplus = document.getElementById("buttonplus");
 var answerbox = document.getElementById("answerbox");
 
+
+button0.addEventListener("click", button0function);
+
+function button0function () {
+    answerbox.innerHTML = 0;
+}
+
+
 button1.addEventListener("click", button1function);
 
 function button1function () {
-    answerbox.innerHTML = "1";
+    answerbox.innerHTML = 1;
 }
 
 button2.addEventListener("click", button2function);
 
 function button2function () {
-    answerbox.innerHTML = "2";
+    answerbox.innerHTML = 2;
 }
+
+button3.addEventListener("click", button3function);
+
+function button3function () {
+    answerbox.innerHTML = 3;
+}
+
+button4.addEventListener("click", button4function);
+
+function button4function () {
+    answerbox.innerHTML = 4;
+}
+button5.addEventListener("click", button5function);
+
+function button5function () {
+    answerbox.innerHTML = 5;
+}
+
+button6.addEventListener("click", button6function);
+
+function button6function () {
+    answerbox.innerHTML = 6;
+}
+button7.addEventListener("click", button7function);
+
+function button7function () {
+    answerbox.innerHTML = 7;
+}
+
+button8.addEventListener("click", button8function);
+
+function button8function () {
+    answerbox.innerHTML = 8;
+}
+button9.addEventListener("click", button9function);
+
+function button9function () {
+    answerbox.innerHTML = 9;
+}
+
+
+
+
+
+
 
 buttonplus.addEventListener("click", buttonplusfunction);
 
 function buttonplusfunction () {
-    answerbox.innerHTML = '+=';
+  answerbox.innerHTML;
 }
 
 buttonequals.addEventListener("click", buttonequalsfunction);
 
 function buttonequalsfunction () {
-    answerbox.innerHTML = "=";
+ answerbox.innerHTML;
 }
-
 
