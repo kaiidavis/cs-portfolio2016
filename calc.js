127b91207f6274976734e4ce1bab0dfe9338721a
schoolworkshasta <kdavis.sh@mysummitps.org>
2016-11-03 02:21:04 +0000

    Update

--- calc.js
+++ calc.js
@@ -1,101 +1,38 @@
-//JavaScript File
+// JavaScript File
+var input1 = document.getElementById("input1");
+var input2 = document.getElementById("input2");
+var functions = document.getElementById("dropdown");
+var output = document.getElementById("output");
+var button = document.getElementById("equal");
+var solution;
 
-var buttonpar = document.getElementById("buttonpar");
-var buttonpar2 = document.getElementById("buttonpar2");
-var buttonpercent = document.getElementById("buttonpercent");
-var buttonAC = document.getElementById("buttonAC");
-var button0 = document.getElementById("button0");
-var button1 = document.getElementById("button1");
-var button2 = document.getElementById("button2");
-var button3 = document.getElementById("button3");
-var button4 = document.getElementById("button4");
-var button5 = document.getElementById("button5");
-var button6 = document.getElementById("button6");
-var button7 = document.getElementById("button7");
-var button8 = document.getElementById("button8");
-var button9 = document.getElementById("button9");
-var buttondot = document.getElementById("buttondot");
-var buttonequals = document.getElementById("buttonequals");
-var buttondivide = document.getElementById("buttondivide");
-var buttonx = document.getElementById("buttonx");
-var buttonminus = document.getElementById("buttonminus");
-var buttonplus = document.getElementById("buttonplus");
-var answerbox = document.getElementById("answerbox");
+button.addEventListener("click", buttonfunction);
 
+function buttonfunction() {
+    if (functions.value == "+"){
+        solution = parseInt(input1.value) + parseInt(input2.value);
+        output.innerHTML = solution;
+    }
 
-button0.addEventListener("click", button0function);
-
-function button0function () {
-    answerbox.innerHTML = 0;
+    else if  (functions.value == "-"){
+        solution = parseInt(input1.value) - parseInt(input2.value);
+        output.innerHTML = solution;
+    }
+    else if  (functions.value == "x"){
+        solution = parseInt(input1.value) * parseInt(input2.value);
+        output.innerHTML = solution;
+    }
+    else if  (functions.value == "/"){
+        solution = parseInt(input1.value) / parseInt(input2.value);
+        output.innerHTML = solution;
+    }
+    else if  (functions.value == "√"){
+        solution = Math.sqrt(parseInt(input2.value));
+        output.innerHTML = solution;
+    }
+    else if  (functions.value == "^"){
+        solution = Math.pow(parseInt(input1.value), parseInt(input2.value));
+        output.innerHTML = solution;
 }
-
-
-button1.addEventListener("click", button1function);
-
-function button1function () {
-    answerbox.innerHTML = 1;
-}
-
-button2.addEventListener("click", button2function);
-
-function button2function () {
-    answerbox.innerHTML = 2;
-}
-
-button3.addEventListener("click", button3function);
-
-function button3function () {
-    answerbox.innerHTML = 3;
-}
-
-button4.addEventListener("click", button4function);
-
-function button4function () {
-    answerbox.innerHTML = 4;
-}
-button5.addEventListener("click", button5function);
-
-function button5function () {
-    answerbox.innerHTML = 5;
-}
-
-button6.addEventListener("click", button6function);
-
-function button6function () {
-    answerbox.innerHTML = 6;
-}
-button7.addEventListener("click", button7function);
-
-function button7function () {
-    answerbox.innerHTML = 7;
-}
-
-button8.addEventListener("click", button8function);
-
-function button8function () {
-    answerbox.innerHTML = 8;
-}
-button9.addEventListener("click", button9function);
-
-function button9function () {
-    answerbox.innerHTML = 9;
-}
-
-
-
-
-
-
-
-buttonplus.addEventListener("click", buttonplusfunction);
-
-function buttonplusfunction () {
-  answerbox.innerHTML;
-}
-
-buttonequals.addEventListener("click", buttonequalsfunction);
-
-function buttonequalsfunction () {
- answerbox.innerHTML;
 }
 
