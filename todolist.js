f12b6b04c5f5292940336252c8236915b0a92ad4
schoolworkshasta <kdavis.sh@mysummitps.org>
2016-11-09 20:48:12 +0000

    Update

--- todolist.js
+++ todolist.js
@@ -10,20 +10,15 @@ var goldborderoutput = document.getElementById("goldborderoutput");
 var redoutput = document.getElementById("redoutput");
 var boldoutput = document.getElementById("boldoutput");
 var underlineoutput = document.getElementById("underlineoutput");
-//These two make button elements for removing things from the list and to make a strikethrough and it is used for each dropdown selection
-var regularremovingbutton = document.createElement('button');
+//Each of these commands make button elements for removing things from the list by using createElement which creates a button element and it is used to make a strikethrough from text deocration in function section after the if and else if statements and it is used for each dropdown selection which is why there is five.
 var regularstrikebtn = document.createElement('button');
 
-var goldborderremovingbutton = document.createElement('button');
 var goldborderstrikebtn = document.createElement('button');
 
-var redremovingbutton = document.createElement('button');
 var redstrikebtn = document.createElement('button');
 
-var boldremovingbutton = document.createElement('button');
 var boldstrikebtn = document.createElement('button');
 
-var underlineremovingbutton = document.createElement('button');
 var underlinestrikebtn = document.createElement('button');
 
 
@@ -41,7 +36,6 @@ function buttonfunction() {
 //This line is used to make the inputbox go back to empty after hitting the sumbitbutton and putting the text into the list. 
         todolistinputbox.value = "";
 //These two lines are putting the button add on using appendChild in the output area. The parenthesis is from the var for each button when it was creating elements written in the var section. The button is a html aspect which is why it has .innerHTML. After that it equals to whatever you want the text to be.
-regularoutput.appendChild(regularremovingbutton).innerHTML = "Click button to take safety off";
 regularoutput.appendChild(regularstrikebtn).innerHTML = "Click button to make the list strikethrough and remove safety";
     }
 //These are the same except for the different dropdown selections making different features. They all use style to add a certain style to the text in the list being made. Using border color or background color or fontWeigt or textDecoration for each of the features that each dropdown section do.
@@ -52,7 +46,6 @@ regularoutput.appendChild(regularstrikebtn).innerHTML = "Click button to make th
         goldborderoutput.style.backgroundColor = "white";
         goldborderoutput.style.textDecoration = "";
         todolistinputbox.value = "";
-goldborderoutput.appendChild(goldborderremovingbutton).innerHTML = "Click button to take safety off";
 goldborderoutput.appendChild(goldborderstrikebtn).innerHTML = "Click button to make the list strikethrough and remove safety";
     }
     else if (dropdown.value === "red") {
@@ -62,7 +55,6 @@ goldborderoutput.appendChild(goldborderstrikebtn).innerHTML = "Click button to m
         redoutput.style.backgroundColor = "white";
         redoutput.style.textDecoration = "";
         todolistinputbox.value = "";
-redoutput.appendChild(redremovingbutton).innerHTML = "Click button to take safety off";
 redoutput.appendChild(redstrikebtn).innerHTML = "Click button to make the list strikethrough and remove safety";
     }
     else if (dropdown.value === "b") {
@@ -72,7 +64,6 @@ redoutput.appendChild(redstrikebtn).innerHTML = "Click button to make the list s
         boldoutput.style.backgroundColor = "white";
         boldoutput.style.textDecoration ="";
         todolistinputbox.value = "";
-boldoutput.appendChild(boldremovingbutton).innerHTML = "Click button to take safety off";
 boldoutput.appendChild(boldstrikebtn).innerHTML = "Click button to make the list strikethrough and remove safety";
     }
     else if (dropdown.value === "underline") {
@@ -81,25 +72,18 @@ boldoutput.appendChild(boldstrikebtn).innerHTML = "Click button to make the list
         underlineoutput.style.textDecoration = "underline";
         underlineoutput.style.backgroundColor = "white";
         todolistinputbox.value = "";
-underlineoutput.appendChild(underlineremovingbutton).innerHTML = "Click button to take safety off";
 underlineoutput.appendChild(underlinestrikebtn).innerHTML = "Click button to make the list strikethrough and remove safety";
     }
 }
-//This listens for when the strike button is clicked and it runs the regularstrikefunction. The evt.target gives back a element to get rid of the child, the button add on function. It uses the first addEventListener as a way to make a safety so that you can't remove something from the list without pushing the button.
+//This listens for when the strike button is clicked and it runs the regularstrikefunction. The evt.target gives back a element to get rid of the child, the button add on function. It uses the first addEventListener to listen for when the button is clicked to activate the function which makes the strikethrough.
 regularstrikebtn.addEventListener("click", regularstrikefunction);
 function regularstrikefunction (evt) {
+//It uses style text decoration to make the strikethrough on the output which is called regularoutput
 regularoutput.style.textDecoration = "line-through";
 var removes = evt.target;
-//This part uses the evt.target to remove stuff from the to do list when you click in the regular output div.
+//This part uses the evt.target to remove child which removes the button from the to do list when the function is activaed and it uses "in regularoutput..." so that it also listens for a click on the div and if there is a click, it reactivaes the remove child command to get rid of the section in the div that is clicked. 
 removes.parentNode.removeChild(removes) in regularoutput.addEventListener("click", regularstrikefunction);
 
-}
-//This is the same thing as the strike button although it takes the safety away without making a strikethrough, while strike button uses text decoration to make these actions. 
-regularremovingbutton.addEventListener("click", regularremovefunction);
-function regularremovefunction (evt){
-    var remove = evt.target;
-    //This is the same as the previous in waiting for a click in the output div, after the safety is removed by using the button.
-remove.parentNode.removeChild(remove) in regularoutput.addEventListener("click", regularremovefunction);
 }
 
 //These are all the same expect they are made for each possible feature.
@@ -110,11 +94,6 @@ var removes = evt.target;
 removes.parentNode.removeChild(removes) in goldborderoutput.addEventListener("click", goldborderstrikefunction);
 
 }
-goldborderremovingbutton.addEventListener("click", goldborderremovefunction);
-function goldborderremovefunction (evt){
-    var remove = evt.target;
-remove.parentNode.removeChild(remove) in goldborderoutput.addEventListener("click", goldborderremovefunction);
-}
 
 redstrikebtn.addEventListener("click", redstrikefunction);
 function redstrikefunction (evt) {
@@ -122,11 +101,6 @@ redoutput.style.textDecoration = "line-through";
 var removes = evt.target;
 removes.parentNode.removeChild(removes) in redoutput.addEventListener("click", redstrikefunction);
 
-}
-redremovingbutton.addEventListener("click", redremovefunction);
-function redremovefunction (evt){
-    var remove = evt.target;
-remove.parentNode.removeChild(remove) in redoutput.addEventListener("click", redremovefunction);
 }
 
 boldstrikebtn.addEventListener("click", boldstrikefunction);
@@ -136,11 +110,6 @@ var removes = evt.target;
 removes.parentNode.removeChild(removes) in boldoutput.addEventListener("click", boldstrikefunction);
 
 }
-boldremovingbutton.addEventListener("click", boldremovefunction);
-function boldremovefunction (evt){
-    var remove = evt.target;
-remove.parentNode.removeChild(remove) in boldoutput.addEventListener("click", boldremovefunction);
-}
 
 underlinestrikebtn.addEventListener("click", underlinestrikefunction);
 function underlinestrikefunction (evt) {
@@ -149,11 +118,7 @@ var removes = evt.target;
 removes.parentNode.removeChild(removes) in underlineoutput.addEventListener("click", underlinestrikefunction);
 
 }
-underlineremovingbutton.addEventListener("click", underlineremovefunction);
-function underlineremovefunction (evt){
-    var remove = evt.target;
-remove.parentNode.removeChild(remove) in underlineoutput.addEventListener("click", underlineremovefunction);
-}
+
 //This function listens for when the mouse is over the output div and waits for when there is no text content so that the background color of white being made when things are being added to the list go away when the mouse goes over it when the text is all deleted.
 regularoutput.addEventListener("mouseover", regularinvisibleborderfunction);
 function regularinvisibleborderfunction () {
