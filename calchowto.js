d89372c75c85c2646fb8fe3260318231fad263f9
schoolworkshasta <kdavis.sh@mysummitps.org>
2016-11-10 23:23:34 +0000

    Update

---
+++ calchowto.js
@@ -0,0 +1,169 @@
+var html1 = document.getElementById("html1");
+var divhtml1 = document.getElementById("divhtml1");
+
+var html2 = document.getElementById("html2");
+var divhtml2 = document.getElementById("divhtml2");
+
+var html3 = document.getElementById("html3");
+var divhtml3 = document.getElementById("divhtml3");
+
+var html4 = document.getElementById("html4");
+var divhtml4 = document.getElementById("divhtml4");
+
+var html5 = document.getElementById("html5");
+var divhtml5 = document.getElementById("divhtml5");
+
+var html6 = document.getElementById("html6");
+var divhtml6 = document.getElementById("divhtml6");
+
+var html7 = document.getElementById("html7");
+var divhtml7 = document.getElementById("divhtml7");
+
+var html8 = document.getElementById("html8");
+var divhtml8 = document.getElementById("divhtml8");
+
+var html9 = document.getElementById("html9");
+var divhtml9 = document.getElementById("divhtml9");
+
+var html10 = document.getElementById("html10");
+var divhtml10 = document.getElementById("divhtml10");
+
+var html11 = document.getElementById("html11");
+var divhtml11 = document.getElementById("divhtml11");
+
+var html12 = document.getElementById("html12");
+var divhtml12 = document.getElementById("divhtml12");
+
+var html13 = document.getElementById("html13");
+var divhtml13 = document.getElementById("divhtml13");
+
+var html14 = document.getElementById("html14");
+var divhtml14 = document.getElementById("divhtml14");
+
+var html15 = document.getElementById("html15");
+var divhtml15 = document.getElementById("divhtml15");
+
+
+
+html1.addEventListener("mouseover", functionhtml1);
+function functionhtml1() {
+   divhtml1.innerHTML =  "<textarea class='thinner-black-border' rows='3' cols='120'>&lt;link rel=&quot;stylesheet&quot; href=&quot;https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css&quot; integrity=&quot;sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u&quot; crossorigin=&quot;anonymous&quot;&gt;<br>&lt;link rel=&quot;stylesheet&quot; href=&quot;mainstyle.css&quot; type=&quot;text/css&quot; /&gt;<br>&lt;link href=&quot;https://fonts.googleapis.com/css?family=Faster+One&quot; rel=&quot;stylesheet&quot;&gt;</textarea>";
+}
+html1.addEventListener("mouseout", functionouthtml1);
+function functionouthtml1() {
+  divhtml1.innerHTML = "";
+}
+
+html2.addEventListener("mouseover", functionhtml2);
+function functionhtml2() {
+   divhtml2.innerHTML =  "<textarea class='thinner-black-border' rows='3' cols='110'>.calculatorbackcolor { background: linear-gradient(315deg, transparent 75%, #d45d55 0)-10px 0, linear-gradient(45deg, transparent 75%, #d45d55 0)-10px 0, linear-gradient(135deg, #a7332b 50%, transparent 0) 0 0, linear-gradient(45deg, #6a201b 50%, #561a16 0) 0 0 #561a16; background-size: 20px 20px; }</textarea>";
+}
+html2.addEventListener("mouseout", functionouthtml2);
+function functionouthtml2() {
+  divhtml2.innerHTML = "";
+}
+
+html3.addEventListener("mouseover", functionhtml3);
+function functionhtml3() {
+  divhtml3.innerHTML = "<textarea class='thinner-black-border' rows='9' cols='60'>&lt;ul id=&quot;navbar&quot;&gt; &lt;li&gt; &lt;a href=&quot;index.html&quot;&gt; About Me &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;hardware.html&quot;&gt; Hardware &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;howto.html&quot;&gt; How To Page &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;calc.html&quot;&gt; Calculator &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;todolist.html&quot;&gt; Todo List &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;calchowto.html&quot;&gt; Calculator How To Page &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;#&quot;&gt; Vindinium &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;#&quot;&gt; Big Project &lt;/a&gt;&lt;/li&gt; &lt;/ul&gt;</textarea>";
+}
+html3.addEventListener("mouseout", functionouthtml3);
+function functionouthtml3() {
+  divhtml3.innerHTML = "";
+}
+html4.addEventListener("mouseover", functionhtml4);
+function functionhtml4() {
+  divhtml4.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='25'>.thin-red-border { border-color: #A93226; border-width: 10px; border-style: solid; border-radius: 10px; background-color: white; } .header { font-family: Faster One; color: darkblue; }</textarea>";
+}
+html4.addEventListener("mouseout", functionouthtml4);
+function functionouthtml4() {
+  divhtml4.innerHTML = "";
+}
+html5.addEventListener("mouseover", functionhtml5);
+function functionhtml5() {
+  divhtml5.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='25'>.thin-blue-border { border-color: #00264d; border-width: 10px; border-style: solid; border-radius: 10px; background-color: white; }</textarea>";
+}
+html5.addEventListener("mouseout", functionouthtml5);
+function functionouthtml5() {
+  divhtml5.innerHTML = "";
+}
+html6.addEventListener("mouseover", functionhtml6);
+function functionhtml6() {
+  divhtml6.innerHTML = "<textarea class='thinner-black-border' rows='15' cols='25'>.inputcenter { position: relative; top: 40%; left: 40%; } .thinner-black-border { border-color: black; border-width: 3px; border-style: solid; border-radius: 3px; background-color: white; } .boldtext { font-weight: bolder; } .calculatorspan { width: 316px; display: block; height: 69px; } .textcalculator { font-size: 40px; } .center { text-align: center; } </textarea>";
+}
+html6.addEventListener("mouseout", functionouthtml6);
+function functionouthtml6() {
+  divhtml6.innerHTML = "";
+}
+html7.addEventListener("mouseover", functionhtml7);
+function functionhtml7() {
+  divhtml7.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='25'>.inputcenter1 { position: relative; top: 40%; left: 20%; { .inputcenter3 { position: relative; top: 40%; left: 36%; } .inputsize { width: 200px; height: 100px; } .textcalculator { font-size: 40px; }</textarea>";
+}
+html7.addEventListener("mouseout", functionouthtml7);
+function functionouthtml7() {
+  divhtml7.innerHTML = "";
+}
+html8.addEventListener("mouseover", functionhtml8);
+function functionhtml8() {
+  divhtml8.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='60'>&lt;select class=&quot;inputcenter2 textcalculator&quot; id=&quot;functiondropdown&quot;&gt; &lt;option value=&quot;+&quot;&gt;Addition&lt;/option&gt; &lt;option value=&quot;-&quot;&gt;Subtraction&lt;/option&gt; &lt;option value=&quot;x&quot;&gt;Multiplication&lt;/option&gt; &lt;option value=&quot;/&quot;&gt;Division&lt;/option&gt; &lt;option value=&quot;^&quot;&gt;Exponent&lt;/option&gt; &lt;option value=&quot;&radic;&quot;&gt;Square Root&lt;/option&gt; &lt;/select&gt; </textarea>";
+}
+html8.addEventListener("mouseout", functionouthtml8);
+function functionouthtml8() {
+  divhtml8.innerHTML = "";
+}
+html9.addEventListener("mouseover", functionhtml9);
+function functionhtml9() {
+  divhtml9.innerHTML = "<textarea class='thinner-black-border' rows='2' cols='70'> &lt;/br&gt; &lt;button class=&quot;btn-lg buttoncenter&quot; id=&quot;equalsbutton&quot;&gt;Answer&lt;/button&gt;</textarea>";
+}
+html9.addEventListener("mouseout", functionouthtml9);
+function functionouthtml9() {
+  divhtml9.innerHTML = "";
+}
+html10.addEventListener("mouseover", functionhtml10);
+function functionhtml10() {
+  divhtml10.innerHTML = "<textarea class='thinner-black-border' rows='5' cols='60'>var firstvalue = document.getElementById(&quot;firstvalue&quot;); var secondvalue = document.getElementById(&quot;secondvalue&quot;); var functiondropdown = document.getElementById(&quot;functiondropdown&quot;); var answeroutput = document.getElementById(&quot;answeroutput&quot;); var equalsbutton = document.getElementById(&quot;equalsbutton&quot;);</textarea>";
+}
+html10.addEventListener("mouseout", functionouthtml10);
+function functionouthtml10() {
+  divhtml10.innerHTML = "";
+}
+html11.addEventListener("mouseover", functionhtml11);
+function functionhtml11() {
+  divhtml11.innerHTML = "<textarea class='thinner-black-border' cols='50' rows='1'>equalsbutton.addEventListener(&quot;click&quot;, buttonfunction); </textarea>";
+}
+html11.addEventListener("mouseout", functionouthtml11);
+function functionouthtml11() {
+  divhtml11.innerHTML = "";
+}
+html12.addEventListener("mouseover", functionhtml12);
+function functionhtml12() {
+  divhtml12.innerHTML = "<textarea class='thinner-black-border' rows='2' cols='40'> var value1 = parseInt(firstvalue.value); var value2 = parseInt(secondvalue.value);</textarea>";
+}
+html12.addEventListener("mouseout", functionouthtml12);
+function functionouthtml12() {
+  divhtml12.innerHTML = "";
+}
+html13.addEventListener("mouseover", functionhtml13);
+function functionhtml13() {
+  divhtml13.innerHTML = "<textarea class='thinner-black-border'> if (functiondropdown.value == &quot;-&quot;){ answeroutput.innerHTML = value1 - value2; }</textarea>";
+}
+html13.addEventListener("mouseout", functionouthtml13);
+function functionouthtml13() {
+  divhtml13.innerHTML = "";
+}
+html14.addEventListener("mouseover", functionhtml14);
+function functionhtml14() {
+  divhtml4.innerHTML = "<textarea class='thinner-black-border' rows='7' cols='40'> else if  (functiondropdown.value == &quot;+&quot;){ answeroutput.innerHTML = value1 + value2; } else if  (functiondropdown.value == &quot;/&quot;){ answeroutput.innerHTML = value1 / value2; } else if  (functiondropdown.value == &quot;x&quot;){ answeroutput.innerHTML = value1 * value2; }</textarea>";
+}
+html14.addEventListener("mouseout", functionouthtml14);
+function functionouthtml14() {
+  divhtml14.innerHTML = "";
+}
+html15.addEventListener("mouseover", functionhtml15);
+function functionhtml15() {
+  divhtml15.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='50'> else if  (functiondropdown.value == &quot;&radic;&quot;){ answeroutput.innerHTML = Math.sqrt(value2); } else if  (functiondropdown.value == &quot;^&quot;){ answeroutput.innerHTML = Math.pow(value1, value2); }</textarea>";
+}
+html15.addEventListener("mouseout", functionouthtml15);
+function functionouthtml15() {
+  divhtml15.innerHTML = "";
+}
