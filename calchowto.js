8675c9414e5c217094bbaf6e2992e3c028970360
schoolworkshasta <kdavis.sh@mysummitps.org>
2016-11-11 00:37:54 +0000

    Update

--- calchowto.js
+++ calchowto.js
@@ -47,9 +47,9 @@ var divhtml15 = document.getElementById("divhtml15");
 
 html1.addEventListener("mouseover", functionhtml1);
 function functionhtml1() {
-   divhtml1.innerHTML =  "<textarea class='thinner-black-border' rows='3' cols='120'>&lt;link rel=&quot;stylesheet&quot; href=&quot;https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css&quot; integrity=&quot;sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u&quot; crossorigin=&quot;anonymous&quot;&gt;<br>&lt;link rel=&quot;stylesheet&quot; href=&quot;mainstyle.css&quot; type=&quot;text/css&quot; /&gt;<br>&lt;link href=&quot;https://fonts.googleapis.com/css?family=Faster+One&quot; rel=&quot;stylesheet&quot;&gt;</textarea>";
+   divhtml1.innerHTML =  "<textarea class='thinner-black-border' rows='4' cols='120'>&lt;link rel=&quot;stylesheet&quot; href=&quot;https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css&quot; integrity=&quot;sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u&quot; crossorigin=&quot;anonymous&quot;&gt;  &#10;&lt;link rel=&quot;stylesheet&quot; href=&quot;mainstyle.css&quot; type=&quot;text/css&quot; /&gt; &#10;&lt;link href=&quot;https://fonts.googleapis.com/css?family=Faster+One&quot; rel=&quot;stylesheet&quot;&gt;</textarea>";
 }
-html1.addEventListener("mouseout", functionouthtml1);
+html1.addEventListener("click", functionouthtml1);
 function functionouthtml1() {
   divhtml1.innerHTML = "";
 }
@@ -58,72 +58,72 @@ html2.addEventListener("mouseover", functionhtml2);
 function functionhtml2() {
    divhtml2.innerHTML =  "<textarea class='thinner-black-border' rows='3' cols='110'>.calculatorbackcolor { background: linear-gradient(315deg, transparent 75%, #d45d55 0)-10px 0, linear-gradient(45deg, transparent 75%, #d45d55 0)-10px 0, linear-gradient(135deg, #a7332b 50%, transparent 0) 0 0, linear-gradient(45deg, #6a201b 50%, #561a16 0) 0 0 #561a16; background-size: 20px 20px; }</textarea>";
 }
-html2.addEventListener("mouseout", functionouthtml2);
+html2.addEventListener("click", functionouthtml2);
 function functionouthtml2() {
   divhtml2.innerHTML = "";
 }
 
 html3.addEventListener("mouseover", functionhtml3);
 function functionhtml3() {
-  divhtml3.innerHTML = "<textarea class='thinner-black-border' rows='9' cols='60'>&lt;ul id=&quot;navbar&quot;&gt; &lt;li&gt; &lt;a href=&quot;index.html&quot;&gt; About Me &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;hardware.html&quot;&gt; Hardware &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;howto.html&quot;&gt; How To Page &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;calc.html&quot;&gt; Calculator &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;todolist.html&quot;&gt; Todo List &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;calchowto.html&quot;&gt; Calculator How To Page &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;#&quot;&gt; Vindinium &lt;/a&gt;&lt;/li&gt; &lt;li&gt; &lt;a href=&quot;#&quot;&gt; Big Project &lt;/a&gt;&lt;/li&gt; &lt;/ul&gt;</textarea>";
+  divhtml3.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='60'>&lt;ul id=&quot;navbar&quot;&gt; &#10; &lt;li&gt; &lt;a href=&quot;index.html&quot;&gt; About Me &lt;/a&gt;&lt;/li&gt; &#10;&lt;li&gt; &lt;a href=&quot;hardware.html&quot;&gt; Hardware &lt;/a&gt;&lt;/li&gt; &#10;&lt;li&gt; &lt;a href=&quot;howto.html&quot;&gt; How To Page &lt;/a&gt;&lt;/li&gt; &#10;&lt;li&gt; &lt;a href=&quot;calc.html&quot;&gt; Calculator &lt;/a&gt;&lt;/li&gt; &#10;&lt;li&gt; &lt;a href=&quot;todolist.html&quot;&gt; Todo List &lt;/a&gt;&lt;/li&gt; &#10; &lt;li&gt; &lt;a href=&quot;#&quot;&gt; Vindinium &lt;/a&gt;&lt;/li&gt; &#10;&lt;li&gt; &lt;a href=&quot;#&quot;&gt; Big Project &lt;/a&gt;&lt;/li&gt; &#10;&lt;/ul&gt;</textarea>";
 }
-html3.addEventListener("mouseout", functionouthtml3);
+html3.addEventListener("click", functionouthtml3);
 function functionouthtml3() {
   divhtml3.innerHTML = "";
 }
 html4.addEventListener("mouseover", functionhtml4);
 function functionhtml4() {
-  divhtml4.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='25'>.thin-red-border { border-color: #A93226; border-width: 10px; border-style: solid; border-radius: 10px; background-color: white; } .header { font-family: Faster One; color: darkblue; }</textarea>";
+  divhtml4.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='35'>.thin-red-border { &#10;border-color: #A93226; &#10;border-width: 10px; &#10;border-style: solid; &#10;border-radius: 10px; &#10;background-color: white; &#10;} &#10;.header { &#10;font-family: Faster One; &#10;color: darkblue; &#10;}</textarea>";
 }
-html4.addEventListener("mouseout", functionouthtml4);
+html4.addEventListener("click", functionouthtml4);
 function functionouthtml4() {
   divhtml4.innerHTML = "";
 }
 html5.addEventListener("mouseover", functionhtml5);
 function functionhtml5() {
-  divhtml5.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='25'>.thin-blue-border { border-color: #00264d; border-width: 10px; border-style: solid; border-radius: 10px; background-color: white; }</textarea>";
+  divhtml5.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='25'>.thin-blue-border { &#10;border-color: #00264d; &#10;border-width: 10px; &#10;border-style: solid; &#10;border-radius: 10px; &#10;background-color: white; &#10;}</textarea>";
 }
-html5.addEventListener("mouseout", functionouthtml5);
+html5.addEventListener("click", functionouthtml5);
 function functionouthtml5() {
   divhtml5.innerHTML = "";
 }
 html6.addEventListener("mouseover", functionhtml6);
 function functionhtml6() {
-  divhtml6.innerHTML = "<textarea class='thinner-black-border' rows='15' cols='25'>.inputcenter { position: relative; top: 40%; left: 40%; } .thinner-black-border { border-color: black; border-width: 3px; border-style: solid; border-radius: 3px; background-color: white; } .boldtext { font-weight: bolder; } .calculatorspan { width: 316px; display: block; height: 69px; } .textcalculator { font-size: 40px; } .center { text-align: center; } </textarea>";
+  divhtml6.innerHTML = "<textarea class='thinner-black-border' rows='15' cols='25'>.inputcenter { &#10;position: relative; &#10;top: 40%; &#10;left: 40%; &#10;}&#10; .thinner-black-border { &#10;border-color: black; &#10;border-width: 3px; &#10;border-style: solid; &#10;border-radius: 3px; &#10;background-color: white; &#10;} &#10;.boldtext { &#10;font-weight: bolder; &#10;} &#10;.&#10;calculatorspan { &#10;width: 316px; &#10;display: block; &#10;height: 69px; &#10;} &#10;.textcalculator { &#10;font-size: 40px; &#10;} &#10;.center { &#10;text-align: center; &#10;} </textarea>";
 }
-html6.addEventListener("mouseout", functionouthtml6);
+html6.addEventListener("click", functionouthtml6);
 function functionouthtml6() {
   divhtml6.innerHTML = "";
 }
 html7.addEventListener("mouseover", functionhtml7);
 function functionhtml7() {
-  divhtml7.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='25'>.inputcenter1 { position: relative; top: 40%; left: 20%; { .inputcenter3 { position: relative; top: 40%; left: 36%; } .inputsize { width: 200px; height: 100px; } .textcalculator { font-size: 40px; }</textarea>";
+  divhtml7.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='25'>.inputcenter1 { &#10;position: relative; &#10;top: 40%; &#10;left: 20%; &#10;{ &#10;.inputcenter3 { &#10;position: relative; &#10;top: 40%; &#10;left: 36%; &#10;} &#10;.inputsize { &#10;width: 200px; &#10;height: 100px; &#10;} &#10;.textcalculator { &#10;font-size: 40px; &#10;}</textarea>";
 }
-html7.addEventListener("mouseout", functionouthtml7);
+html7.addEventListener("click", functionouthtml7);
 function functionouthtml7() {
   divhtml7.innerHTML = "";
 }
 html8.addEventListener("mouseover", functionhtml8);
 function functionhtml8() {
-  divhtml8.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='60'>&lt;select class=&quot;inputcenter2 textcalculator&quot; id=&quot;functiondropdown&quot;&gt; &lt;option value=&quot;+&quot;&gt;Addition&lt;/option&gt; &lt;option value=&quot;-&quot;&gt;Subtraction&lt;/option&gt; &lt;option value=&quot;x&quot;&gt;Multiplication&lt;/option&gt; &lt;option value=&quot;/&quot;&gt;Division&lt;/option&gt; &lt;option value=&quot;^&quot;&gt;Exponent&lt;/option&gt; &lt;option value=&quot;&radic;&quot;&gt;Square Root&lt;/option&gt; &lt;/select&gt; </textarea>";
+  divhtml8.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='60'>&lt;select class=&quot;inputcenter2 textcalculator&quot; id=&quot;functiondropdown&quot;&gt; &#10;&lt;option value=&quot;+&quot;&gt;Addition&lt;/option&gt; &#10;&lt;option value=&quot;-&quot;&gt;Subtraction&lt;/option&gt; &#10;&lt;option value=&quot;x&quot;&gt;Multiplication&lt;/option&gt; &#10;&lt;option value=&quot;/&quot;&gt;Division&lt;/option&gt; &#10;&lt;option value=&quot;^&quot;&gt;Exponent&lt;/option&gt; &#10;&lt;option value=&quot;&radic;&quot;&gt;Square Root&lt;/option&gt; &#10;&lt;/select&gt; </textarea>";
 }
-html8.addEventListener("mouseout", functionouthtml8);
+html8.addEventListener("click", functionouthtml8);
 function functionouthtml8() {
   divhtml8.innerHTML = "";
 }
 html9.addEventListener("mouseover", functionhtml9);
 function functionhtml9() {
-  divhtml9.innerHTML = "<textarea class='thinner-black-border' rows='2' cols='70'> &lt;/br&gt; &lt;button class=&quot;btn-lg buttoncenter&quot; id=&quot;equalsbutton&quot;&gt;Answer&lt;/button&gt;</textarea>";
+  divhtml9.innerHTML = "<textarea class='thinner-black-border' rows='2' cols='70'>&lt;/br&gt; &#10;&lt;button class=&quot;btn-lg buttoncenter&quot; id=&quot;equalsbutton&quot;&gt;Answer&lt;/button&gt;</textarea>";
 }
-html9.addEventListener("mouseout", functionouthtml9);
+html9.addEventListener("click", functionouthtml9);
 function functionouthtml9() {
   divhtml9.innerHTML = "";
 }
 html10.addEventListener("mouseover", functionhtml10);
 function functionhtml10() {
-  divhtml10.innerHTML = "<textarea class='thinner-black-border' rows='5' cols='60'>var firstvalue = document.getElementById(&quot;firstvalue&quot;); var secondvalue = document.getElementById(&quot;secondvalue&quot;); var functiondropdown = document.getElementById(&quot;functiondropdown&quot;); var answeroutput = document.getElementById(&quot;answeroutput&quot;); var equalsbutton = document.getElementById(&quot;equalsbutton&quot;);</textarea>";
+  divhtml10.innerHTML = "<textarea class='thinner-black-border' rows='5' cols='60'>var firstvalue = document.getElementById(&quot;firstvalue&quot;); &#10;var secondvalue = document.getElementById(&quot;secondvalue&quot;); &#10;var functiondropdown = document.getElementById(&quot;functiondropdown&quot;); &#10;var answeroutput = document.getElementById(&quot;answeroutput&quot;); &#10;var equalsbutton = document.getElementById(&quot;equalsbutton&quot;);</textarea>";
 }
-html10.addEventListener("mouseout", functionouthtml10);
+html10.addEventListener("click", functionouthtml10);
 function functionouthtml10() {
   divhtml10.innerHTML = "";
 }
@@ -131,39 +131,39 @@ html11.addEventListener("mouseover", functionhtml11);
 function functionhtml11() {
   divhtml11.innerHTML = "<textarea class='thinner-black-border' cols='50' rows='1'>equalsbutton.addEventListener(&quot;click&quot;, buttonfunction); </textarea>";
 }
-html11.addEventListener("mouseout", functionouthtml11);
+html11.addEventListener("click", functionouthtml11);
 function functionouthtml11() {
   divhtml11.innerHTML = "";
 }
 html12.addEventListener("mouseover", functionhtml12);
 function functionhtml12() {
-  divhtml12.innerHTML = "<textarea class='thinner-black-border' rows='2' cols='40'> var value1 = parseInt(firstvalue.value); var value2 = parseInt(secondvalue.value);</textarea>";
+  divhtml12.innerHTML = "<textarea class='thinner-black-border' rows='2' cols='40'>var value1 = parseInt(firstvalue.value); &#10;var value2 = parseInt(secondvalue.value);</textarea>";
 }
-html12.addEventListener("mouseout", functionouthtml12);
+html12.addEventListener("click", functionouthtml12);
 function functionouthtml12() {
   divhtml12.innerHTML = "";
 }
 html13.addEventListener("mouseover", functionhtml13);
 function functionhtml13() {
-  divhtml13.innerHTML = "<textarea class='thinner-black-border'> if (functiondropdown.value == &quot;-&quot;){ answeroutput.innerHTML = value1 - value2; }</textarea>";
+  divhtml13.innerHTML = "<textarea class='thinner-black-border' rows='3' cols='35'>if (functiondropdown.value == &quot;-&quot;){ answeroutput.innerHTML = value1 - value2; &#10;}</textarea>";
 }
-html13.addEventListener("mouseout", functionouthtml13);
+html13.addEventListener("click", functionouthtml13);
 function functionouthtml13() {
   divhtml13.innerHTML = "";
 }
 html14.addEventListener("mouseover", functionhtml14);
 function functionhtml14() {
-  divhtml4.innerHTML = "<textarea class='thinner-black-border' rows='7' cols='40'> else if  (functiondropdown.value == &quot;+&quot;){ answeroutput.innerHTML = value1 + value2; } else if  (functiondropdown.value == &quot;/&quot;){ answeroutput.innerHTML = value1 / value2; } else if  (functiondropdown.value == &quot;x&quot;){ answeroutput.innerHTML = value1 * value2; }</textarea>";
+  divhtml14.innerHTML = "<textarea class='thinner-black-border' rows='7' cols='40'>else if (functiondropdown.value == &quot;+&quot;){ &#10;answeroutput.innerHTML = value1 + value2; &#10;} &#10;else if (functiondropdown.value == &quot;/&quot;){ &#10;answeroutput.innerHTML = value1 / value2; &#10;} &#10;else if (functiondropdown.value == &quot;x&quot;){ &#10;answeroutput.innerHTML = value1 * value2; &#10;}</textarea>";
 }
-html14.addEventListener("mouseout", functionouthtml14);
+html14.addEventListener("click", functionouthtml14);
 function functionouthtml14() {
   divhtml14.innerHTML = "";
 }
 html15.addEventListener("mouseover", functionhtml15);
 function functionhtml15() {
-  divhtml15.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='50'> else if  (functiondropdown.value == &quot;&radic;&quot;){ answeroutput.innerHTML = Math.sqrt(value2); } else if  (functiondropdown.value == &quot;^&quot;){ answeroutput.innerHTML = Math.pow(value1, value2); }</textarea>";
+  divhtml15.innerHTML = "<textarea class='thinner-black-border' rows='6' cols='50'>else if (functiondropdown.value == &quot;&radic;&quot;){ &#10;answeroutput.innerHTML = Math.sqrt(value2); &#10;} &#10;else if (functiondropdown.value == &quot;^&quot;){ &#10;answeroutput.innerHTML = Math.pow(value1, value2); &#10;}</textarea>";
 }
-html15.addEventListener("mouseout", functionouthtml15);
+html15.addEventListener("click", functionouthtml15);
 function functionouthtml15() {
   divhtml15.innerHTML = "";
 }
